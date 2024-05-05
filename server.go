4de31b98c1020b6c560576c92e54602dda8a68df
xander <k.ehsan75@gmail.com>
2024-05-05 12:23:00 +0330

    add middlewares

--- server.go
+++ server.go
@@ -2,25 +2,35 @@ package main
 
 import (
 	"go-rest/controller"
+	"go-rest/middleware"
 	"go-rest/service"
+	"io"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 var (
-	videoService service.VideoService=service.New()
+	videoService    service.VideoService       = service.New()
 	videoController controller.VideoController = controller.New(videoService)
-
 )
 
-func main(){
-	server:=gin.Default()
-	server.GET("/videos",func (ctx *gin.Context)  {
-		ctx.JSON(200,videoController.FindAll())
+func setupLogOutput() {
+	f, _ := os.Create("gin.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+}
+
+func main() {
+	setupLogOutput()
+	server := gin.New()
+	server.Use(gin.Recovery(), middleware.Logger(),middleware.BasicAuth())
+
+	server.GET("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.FindAll())
 	})
 
-	server.POST("/videos",func (ctx *gin.Context)  {
-		ctx.JSON(200,videoController.Save(ctx))
+	server.POST("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.Save(ctx))
 	})
 	server.Run(":8080")
 
